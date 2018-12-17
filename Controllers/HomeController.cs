5d3c943c6a723f9ac18c35a11bfb57c9cbc08c70
NarohC <NarohCramhen@gmail.com>
2018-12-17 12:49:31 -0700

    Arrglado link de descarga android

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -34,9 +34,8 @@ namespace AutNutriYA.Controllers
         public IActionResult InfoApp()
         {
             ViewData["Message"] = "Descarga la App";
-            var myUri = new Uri("https://s101moyag8.blob.core.windows.net/app/NutriYA.apk");
-            var uri = myUri.AbsolutePath;
-            ViewData["Link"]= uri;
+            var Link= "https://s101moyag8.blob.core.windows.net/app/NutriYA.apk?st=2018-12-17T19%3A28%3A20Z&se=2019-02-01T06%3A59%3A00Z&sp=r&sv=2018-03-28&sr=b&sig=lXRjRJIQJ2%2FkI7FbzvXoIIhfW9l23siTZ%2B9sHPJ0Bus%3D";
+            ViewData["Link"]= Link;
             //OnActionExecuted
             return View();
         }
